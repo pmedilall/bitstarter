11e9e49d64532882ceb5ee5aa618dcdad78339b0
Pranesh Medilall <pmedilall@gmail.com>
2013-07-17 03:37:22 +0000

    Original + toString test6

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var fs = require('fs');
-    var buffer = new Buffer();
+    var buffer = new Buffer(str,'utf8');
     response.send(fs.readFileSync('index.html').toString());
 });
 
