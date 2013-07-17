56e232c4ff3438a7b06495dde56d9571077b002c
Pranesh Medilall <pmedilall@gmail.com>
2013-07-17 00:46:28 +0000

    Updated web.js - added filesync index.html

--- web.js
+++ web.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send fs.readFile('index.html', function (err, data) {
-  if (err) throw err;
-  console.log(data);
-
+  var fs = require('fs');
+  var buffer = new Buffer("utf-8");
+  response.send(buffer.toString('utc-8', fs.readFileSync("index.html"))); 
+  
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
