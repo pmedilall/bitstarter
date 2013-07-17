63fbf50e58fc2c64919ab0438c1f9a9f0de87cfd
Pranesh Medilall <pmedilall@gmail.com>
2013-07-17 00:19:17 +0000

    Updated web.js - update index.html

--- web.js
+++ web.js
@@ -6,8 +6,6 @@ app.get('/', function(request, response) {
   response.send fs.readFile('index.html', function (err, data) {
   if (err) throw err;
   console.log(data);
-});
-});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
