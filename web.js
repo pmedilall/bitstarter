9410af089fef7595c8cd4b580c578b17637a4a41
Pranesh Medilall <pmedilall@gmail.com>
2013-07-17 03:33:58 +0000

    Original + toString test5

--- web.js
+++ web.js
@@ -6,6 +6,7 @@ app.get('/', function(request, response) {
     var fs = require('fs');
     var buffer = new Buffer();
     response.send(fs.readFileSync('index.html').toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
